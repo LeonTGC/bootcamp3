55b14295bafd5e9cf22668dad465fe1ce39ce2af
Cooper <leon.cooper@gceducationandskills.ac.uk>
2023-04-19 09:56:37 +0100

    added movie checker

--- ifElse.js
+++ ifElse.js
@@ -1,10 +1,37 @@
-let weather = "snow"
+// let weather = "snow"
 
-//  condition is false
-if (weather == "sunny") {
-    console.log("it's sunny outside")
-} else if (weather == "rain") {
-    console.log("get your umbrella") 
+// //  condition is false
+// if (weather == "sunny") {
+//     console.log("it's sunny outside")
+// } else if (weather == "rain") {
+//     console.log("get your umbrella") 
+// } else {
+//     console.log("hate this weather")
+// }
+
+
+
+// SESSION WILL START SHORTLY 
+
+// >>>>>> TASK!!! <<<<<<
+
+// Create conditional statements that checks a variable called "age",
+// if the number value of age is less that 12, log "can see pg movie"
+// if age is less than 15, log "can see pg & 12"
+// if age is less than 18 "can see pg, 12 & 15"
+// anything else, "can view all movies"
+
+// I'll be right back <<<<<<<<<<<<!!!!!<<<<<<<!!!!!!
+
+let age = 18
+
+if (age < 12){
+    console.log("can see pg")
+} else if(age < 15){
+    console.log("can see pg & 12")
+} else if(age < 18){
+    console.log("can see pg, 12 & 15")
 } else {
-    console.log("hate this weather")
+    console.log("can view all movies")
 }
+
