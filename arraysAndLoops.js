8e6b856fe7b10a91ae71b8a30d50ca95da2105e8
Cooper <leon.cooper@gceducationandskills.ac.uk>
2023-04-19 14:14:06 +0100

    added for loop

---
+++ arraysAndLoops.js
@@ -0,0 +1,70 @@
+// Arrays
+// let favSongs = [
+//     "blur - song2",
+//     "eiffel65 - blue da ba dee da ba dai",
+//     "spice girls - wannabe"
+// ]
+// select an item in a specific position
+// favSongs[2] = "black sabbath - iron man"
+
+// methods to add at the end and remove the last item
+// favSongs.push("daft punk - derezzed")
+// favSongs.pop()
+
+// MAP Method
+// let nums = [ 1, 2, 3, 4, 5]
+
+// let result = nums.map((num) => num * 2)
+// console.log(result)
+
+let books = [
+    "36 streets",
+    "ready player one",
+    "lifelik3"
+]
+
+// SHIFT Method
+// console.log(books.shift()) // shows the removed item
+// console.log(books) // shows items left
+
+// UNSHIFT Method
+// books.unshift("the hobbit", "world war z")
+// console.log(books)
+
+// SLICE Method
+// let removedBooks = books.slice(0 , 2)
+// console.log(removedBooks)
+
+// SPLICE Method
+// books.splice(2, 1, "ready player two")
+// console.log(books) 
+
+
+// LOOPS !!!!!!!!!!!
+
+// let favMovies = [
+//     "lord of the rings - the fellowship",
+//     "lord of the rings - two towers",
+//     "blade runner"
+// ]
+
+// for (let i = 0; i < favMovies.length; i++){
+//     console.log(favMovies[i], i)
+// }
+
+
+// let nums = []
+
+// for (let i = 0; i < 6; i++){
+//     nums.push(Math.ceil(Math.random() * 50))
+// }
+// console.log(nums)
+
+let divisbleByTwo = []
+
+for (let i = 1; i <= 20; i++){
+    if(i % 2 === 0){
+        divisbleByTwo.push(i)
+    }
+}
+console.log(divisbleByTwo)
--- ifElse.js
+++ ifElse.js
@@ -68,3 +68,22 @@
 // }
 
 
+// let num = 12
+
+// if (num % 2 === 0){
+//     console.log("even number")
+// } else {
+//     console.log("odd number")
+// }
+
+let num = 1
+
+if (num % 5 === 0 && num % 3 === 0){
+    console.log("divisible by 5 and 3")
+} else if (num % 3 === 0){
+    console.log("divisible by 3")
+} else if (num % 5 === 0){
+    console.log("divisible by 5")
+} else {
+    console.log("not divisible by either 5 or 3")
+}
